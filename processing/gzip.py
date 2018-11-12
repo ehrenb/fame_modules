a9929dc0589829a864cd01c7e6585cd54bf008d0
behrenreich <behrenreich@vencorelabs.com>
2018-11-12 12:50:07 -0800

    testing gzip

---
+++ processing/gzip.py
@@ -0,0 +1,23 @@
+import os
+import gzip
+import shutil
+
+from fame.core.module import ProcessingModule
+from fame.common.utils import tempdir
+
+
+class Gzip(ProcessingModule):
+    name = "gzip"
+    description = "Extract files from GZip archive."
+    acts_on = "gzip"
+
+    def each(self, target):
+        tmpdir = tempdir()
+
+        with gzip.open(target, 'rb') as f_in:
+            filepath = os.path.join(tmpdir, target)
+            with open(filepath, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+                self.add_extracted_file(filepath)
+
+        return True
