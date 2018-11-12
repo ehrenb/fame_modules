983e6316d8ae0e2af7bf6fe151e2c37e4aef135b
behrenreich <behrenreich@vencorelabs.com>
2018-11-12 13:54:36 -0800

    test

--- processing/gz.py
+++ processing/gz.py
@@ -13,10 +13,8 @@ class Gz(ProcessingModule):
 
     def each(self, target):
         tmpdir = tempdir()
-
-        with gzip.open(target, 'rb') as f_in:
-            filepath = os.path.join(tmpdir, target)
-            with open(filepath, 'wb') as f_out:
+        filepath = os.path.join(tmpdir, target)
+        with gzip.open(target, 'rb') as f_in, open(filepath, 'wb') as f_out:
                 shutil.copyfileobj(f_in, f_out)
                 self.add_extracted_file(filepath)
 
