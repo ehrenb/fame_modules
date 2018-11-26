a3a8c2ca284ce4ff6e76d878bcbe722448f7f4dc
behrenreich <behrenreich@vencorelabs.com>
2018-11-26 10:52:57 -0800

    test

--- processing/apk/install.sh
+++ processing/apk/install.sh
@@ -3,4 +3,6 @@
 git clone --recursive https://github.com/rednaga/yara-python-1 yara-python && \
 cd yara-python && \
 python setup.py build --enable-dex install && \
-pip install apkid
+pip install apkid && \
+rm -rf yara-python
+
