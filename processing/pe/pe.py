4a07ecead08f6ddc1325df2601f466f518e00732
behrenreich <behrenreich@vencorelabs.com>
2018-11-26 06:35:35 -0800

    add pefile packet detection

--- processing/pe/pe.py
+++ processing/pe/pe.py
@@ -7,6 +7,13 @@ try:
 except ImportError:
     HAVE_LIEF = False
 
+try: 
+    import peutil
+    HAVE_PEFILE = True
+except ImportError:
+    HAVE_PEFILE = False
+
+
 from fame.core.module import ProcessingModule
 from fame.common.exceptions import ModuleInitializationError
 
@@ -19,13 +26,25 @@ class PE(ProcessingModule):
     def initialize(self):
         if not HAVE_LIEF:
             raise ModuleInitializationError(self, "Missing dependency: lief")
+        if not HAVE_PEFILE:
+            raise ModuleInitializationError(self, "Missing dependency: pefile")
 
     def each(self, target):
         self.results = dict()
         try:
+            # lief analysis
             binary = lief.parse(target)
             binary_dict = json.loads(lief.to_json(binary), parse_int=str)
             self.results.update(binary_dict)
+
+            # packet detect using pefile (PEiD sigs)
+            # sig file obtained here https://github.com/erocarrera/pefile/blob/wiki/PEiDSignatures.md
+            # named after date downloaded & added to FAME
+            signatures = peutils.SignatureDatabase('peid_sigs_11262018.txt')
+            matches = signatures.match(target, ep_only = True)
+            packer_dict {'packers': matches}
+            self.results.update(packer_dict)
+
         except:
             self.log('error', traceback.print_exc())
         return True
