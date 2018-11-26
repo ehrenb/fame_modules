55855de82896e0fe2e1b16f10c1e5b8100e0943e
behrenreich <behrenreich@vencorelabs.com>
2018-11-26 10:44:21 -0800

    add install script for apkid

---
+++ processing/apk/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+git clone --recursive https://github.com/rednaga/yara-python-1 yara-python && \
+cd yara-python && \
+python setup.py build --enable-dex install && \
+pip install apkid
