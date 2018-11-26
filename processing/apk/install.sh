eadcf1378eb6a96be14359ddf5dfceacaed0b043
behrenreich <behrenreich@vencorelabs.com>
2018-11-26 11:33:43 -0800

    test

--- processing/apk/install.sh
+++ processing/apk/install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-source /fame/env/bin/activate && \
 git clone --recursive https://github.com/rednaga/yara-python-1 /tmp/yara-python && \
 cd /tmp/yara-python && \
 python setup.py build --enable-dex install --force && \
