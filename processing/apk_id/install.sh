f6459607bb3bfa6cd0fe4e07c4fec1aeed8db823
behrenreich <behrenreich@vencorelabs.com>
2018-12-03 12:17:20 -0800

    test

--- processing/apk_id/install.sh
+++ processing/apk_id/install.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-# pip install yara-python --global-option="build_ext" --global-option="--enable-dex" &&\
-# pip install apkid --no-deps
-git clone --recursive https://github.com/rednaga/yara-python-1 yara-python &&\
-cd yara-python &&\
-python setup.py build --enable-dex install &&\
-pip install apkid
+pip install yara-python --global-option="build_ext" --global-option="--enable-dex" &&\
+pip install apkid --no-deps
+# git clone --recursive https://github.com/rednaga/yara-python-1 yara-python &&\
+# cd yara-python &&\
+# python setup.py build --enable-dex install &&\
+# pip install apkid
--- processing/bamfdetect/install.sh
+++ processing/bamfdetect/install.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 
-#pip install yara-python --global-option="build_ext" --global-option="--enable-dex"
-git clone --recursive https://github.com/rednaga/yara-python-1 yara-python &&\
-cd yara-python &&\
-python setup.py build --enable-dex install
+pip install yara-python --global-option="build_ext" --global-option="--enable-dex"
