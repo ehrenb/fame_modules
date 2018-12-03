aaafa48761737d0cdfa5ba6048cfa8f8ee897736
behrenreich <behrenreich@vencorelabs.com>
2018-12-03 11:40:20 -0800

    attempt to fix apkid

--- processing/bamfdetect/install.sh
+++ processing/bamfdetect/install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-pip install yara-python --global-option="build_ext" --global-option="--enable-dex"
+#pip install yara-python --global-option="build_ext" --global-option="--enable-dex"
+git clone --recursive https://github.com/rednaga/yara-python-1 yara-python &&\
+cd yara-python &&\
+python setup.py build --enable-dex install
